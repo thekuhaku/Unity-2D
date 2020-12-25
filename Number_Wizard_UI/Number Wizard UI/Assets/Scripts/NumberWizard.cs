df3bf0e647ba4aba042012be31d77b8cda195da1
thekuhaku <thekuhaku25@gmail.com>
2020-12-25 12:30:00 -0700

    First commit

---
+++ Number_Wizard_UI/Number Wizard UI/Assets/Scripts/NumberWizard.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class NumberWizard : MonoBehaviour
+{
+
+    [SerializeField] int max;
+    [SerializeField] int min;
+    [SerializeField] TextMeshProUGUI guessText;
+    int guess;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartGame();
+    }
+
+
+    void StartGame()
+    {
+        NextGuess();
+    }    
+
+    public void OnPressHigher()
+    {
+        min = guess + 1;
+        if (min > max) min = max;
+        NextGuess();
+    }
+
+
+    public void OnPressLower()
+    {
+        max = guess - 1;
+        if (max < min) max = min;
+        NextGuess();
+    }
+
+    public void NextGuess()
+    {
+        guess = Random.Range(min, max + 1);
+        guessText.text = guess.ToString();
+    }
+
+
+}
